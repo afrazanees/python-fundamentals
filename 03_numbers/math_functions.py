b3f199ebccd2372ddc7f89e5d19e457f46af7eb2
Afraz <167850394+afrazanees@users.noreply.github.com>
2026-09-11 14:11:21 +0500

    Add files via upload

---
+++ 03_numbers/math_functions.py
@@ -0,0 +1,30 @@
+# 1. Round Number Function - round the number
+print(round(2.9))
+print(round(2.2))
+
+
+# 2. Absolute Value Function - return the absolute value
+print(abs(-1.2))
+
+# Python have few built-in math functions. If you want to do complex mathematical 
+# calculations, you have to use "Math Module".
+
+# "Module" is like a separate file, which has some python code.
+
+# To use any module, you have to "Import it."
+
+import math 
+# Here "math" is an "Object". So we use functions from "Math module" as we call functions from 
+# "math module" just as same.
+
+# 3. Ciel Function - Gives the closest integer greater than or equal to x
+print(math.ceil(2.2))
+
+# 4. Floor Function - Gives the closest integer lesser than or equal to x
+print(math.floor(2.2))
+
+# 5. Factorial Function - Gives factorial of a number
+print(math.factorial(5))
+
+# 6. Remainder Function - Gives the remainder
+print(math.remainder(10,3))
---
+++ 03_numbers/numbers_and_arithmetic.py
@@ -0,0 +1,44 @@
+# Following are the ways to represent numbers in python
+
+# 1. Simple Number
+x = 1
+
+# 2. Float/Decimal Number
+x = 1.1
+
+# 3. Complex Numbers
+x = 1 + 2j          # Instead of "i" for "Imaginary Number", we use "j" in Python
+
+
+# Standard Arithimatic Operations can also be performed in Python
+# 1. Addition
+print(10 + 3)
+
+# 2. Subraction
+print(10 - 3)
+
+# 3. Multiplication
+print(10 * 3)
+
+# 4. Division
+# In python, we have two types of divison
+# i. To get "Floating Point" number in output
+print(10 / 3)
+# ii. To get "Integer" number in output
+print(10 // 3)
+
+# 5. Moduolus - To get remainder of division
+print(10 % 3)
+
+# 6. Exponent - Which is "LEFT" to the power of "RIGHT"
+print(10 ** 3)          # It is read as "10 to the power of 3"
+
+# 7. Augmented Assignment Operator
+y = 10
+y = 10 + 3
+print(y)
+# We use "Augmented Assignment Operator" for simpler approach
+y += 3
+print(y)
+
+# Any Arithmetic operator can be used with "Augment Asignment Operator"
